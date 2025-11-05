9aab9c35268e431c064b3f639c6cd34159878270
MIkamal88 <109902742+MIkamal88@users.noreply.github.com>
2025-11-05 07:33:15 +0400

    Clean up

--- test_upload.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-curl -X POST "http://localhost:8000/filter-resumes" \
-  -F "files=@resumes/CM Cv 1.pdf" \
-  -F "files=@resumes/CM Cv 2.pdf" \
-  -F 'keywords=["Infrastructure", "Roads", "Highways", "Sewer", "pump station", "pipeline", "storm water", "bridge", "Drainage Networks"]' \
-  -F "min_score=0" \
-  -F "generate_ai_summary=true"
